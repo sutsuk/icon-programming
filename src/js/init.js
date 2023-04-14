eeb103abef4fc6367f07e1949d7e42210269a89a
sutsuk <utsu017@outlook.jp>
2023-04-14 17:45:51 +0900

    Update init.js
    Added functions to generate and place HTML tags.

--- src/js/init.js
+++ src/js/init.js
@@ -10,3 +10,30 @@ function GET(name){
   return params.get(name);
 }
 
+function make_html_header(text){
+}
+
+function make_html_text(value="", className="", readonly=false){
+}
+
+function make_html_check(value="", className=""){
+}
+
+function make_html_option(value=""){
+}
+
+function make_html_ul(value="", className=""){
+}
+
+function make_html_li(value=""){
+}
+
+function make_html_img(id="", className="", src="", hgt="", style=null){
+}
+
+function make_html_button(text="", id="", onclick=""){
+}
+
+function add_html(dom, text){
+}
+
