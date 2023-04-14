766fc7d8c4fd6e36ff92e9b6c0990503f7be187e
sutsuk <utsu017@outlook.jp>
2023-04-14 17:51:10 +0900

    Update init.js
    Added function processing to generate and place HTML tags.

--- src/js/init.js
+++ src/js/init.js
@@ -11,29 +11,65 @@ function GET(name){
 }
 
 function make_html_header(text){
+  var attr = 'class="header" ';
+  return '<p '+attr+'>'+text+'</p>';
 }
 
 function make_html_text(value="", className="", readonly=false){
+  var attr = 'type="text" ';
+  if(className) attr += 'class="'+className+'" ';
+  if(value)     attr += 'value="'+value+'" ';
+  if(readonly)  attr += "readonly";
+  return '<input '+attr+'>';
 }
 
 function make_html_check(value="", className=""){
+  var attr = 'type="checkbox" ';
+  if(className) attr += 'class="'+className+'" ';
+  if(value)     attr += 'value="'+value+'" ';
+  return '<input '+attr+'>';
 }
 
 function make_html_option(value=""){
+  var attr = '';
+  if(value) attr += 'value="'+value+'" ';
+  return '<option '+attr+'>'+value+'</option>';
 }
 
 function make_html_ul(value="", className=""){
+  var attr = '';
+  if(className) attr += 'class="'+className+'" ';
+  return '<ul '+attr+'>'+value+'</ul>';
 }
 
 function make_html_li(value=""){
+  var attr = '';
+  return '<li>'+value+'</li>';
 }
 
 function make_html_img(id="", className="", src="", hgt="", style=null){
+  var attr = "";
+  if(id)    attr += 'id="'+id+'" ';
+  if(src)   attr += 'src="'+src+'" ';
+  if(hgt)   attr += 'height="'+hgt+'px" ';
+  if(style){
+    if(style.top >= 0 && style.left >= 0){
+      className += " abs"
+      attr += 'style="top:'+style.top+'px; left:'+style.left+'px;"';
+    }
+  }
+  if(className) attr += 'class="'+className+'" ';
+  return '<img '+attr+'>';
 }
 
 function make_html_button(text="", id="", onclick=""){
+  var attr = "";
+  if(id)      attr += 'id="'+id+'" ';
+  if(onclick) attr += 'onclick="'+onclick+'" ';
+  return '<button '+attr+'>'+text+'</button>';
 }
 
 function add_html(dom, text){
+  dom.insertAdjacentHTML("beforeEnd", text);
 }
 
