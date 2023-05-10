77dc60d7f149494b4564e871fcd609d9a345e48e
sutsuk <107033259+sutsuk@users.noreply.github.com>
2023-05-10 17:06:03 +0900

    Update attr.js
    Added functions that run events depending on the input.

--- src/js/attr.js
+++ src/js/attr.js
@@ -59,3 +59,63 @@ function init_input_attr(){
   }
 }
 
+function print_change(){
+  console.log("[CALL] print_change()");
+  var display = ["none", "none"]
+  switch($id("print-type").value){
+    case "prm": display[0] = ""; break;
+    case "vle": display[1] = ""; break;
+  }
+  $id("print-param-select").style.display = display[0];
+  $id("print-string-input").style.display = display[1];
+}
+
+function if_change(){
+  console.log("[CALL] if_change()");
+  var display = ["none", "none", "none"];
+  switch($id("if-type").value){
+    case "tof": display = ["none", "none", ""]; break;
+    case "pev": display = ["", "none", ""];     break;
+    case "pep": display = ["", "", "none"];     break;
+  }
+  $id("if-param1-select").style.display = display[0];
+  $id("if-param2-select").style.display = display[1];
+  $id("if-value-input").style.display   = display[2];
+}
+
+function math_change(phase, op=""){
+  console.log("[CALL] math_change()");
+  var type, display;
+  if(phase == 0){
+    display = ["none", "none", "none", "none"];
+    switch($id("math-symbol").value){
+      case "plus":     display[0] = ""; break;
+      case "minus":    display[1] = ""; break;
+      case "times":    display[2] = ""; break;
+      case "division": display[3] = ""; break;
+    }
+    $id("math-plus").style.display     = display[0];
+    $id("math-minus").style.display    = display[1];
+    $id("math-times").style.display    = display[2];
+    $id("math-division").style.display = display[3];
+  }else if(phase == 1){
+    display = ["", "none", "none", "none", "none"];
+    switch(op){
+      case "plus":     type = $id("math-ptype").value; break;
+      case "minus":    type = $id("math-mtype").value; break;
+      case "times":    type = $id("math-ttype").value; break;
+      case "division": type = $id("math-dtype").value; break;
+    }
+    switch(type){
+      case "pp": display[1] = ""; display[2] = ""; break;
+      case "pv": display[1] = ""; display[3] = ""; break;
+      case "vv": display[3] = ""; display[4] = ""; break;
+    }
+    $id("math-param1-select").style.display = display[0];
+    $id("math-param2-select").style.display = display[1];
+    $id("math-param3-select").style.display = display[2];
+    $id("math-value1-input").style.display  = display[3];
+    $id("math-value2-input").style.display  = display[4];
+  }
+}
+
