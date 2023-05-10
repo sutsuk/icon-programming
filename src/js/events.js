7cc5cf35b59066d2343365a1a9a890909a6e2499
sutsuk <utsu017@outlook.jp>
2023-05-10 17:19:16 +0900

    Update events.js
    Added a function to hold icon.

--- src/js/events.js
+++ src/js/events.js
@@ -21,3 +21,29 @@ function icon_summon(tag, x, y, attr){
   data.icons[len_icons].DOM.addEventListener("touchend", icon_release);
 }
 
+function icon_hold(event){
+  console.log("[CALL] icon_hold()");
+  events.DOM = this;
+  target.DOM = this;
+  target.icon = (events.DOM.id).split('-')[1];
+  if(data.icons[target.icon].x >= 0 && data.icons[target.icon].y >= 0){
+    data.table[data.icons[target.icon].y][data.icons[target.icon].x] = -1;
+  }
+  if(event.type == "touchstart"){
+    event = event.changedTouches[0];
+    def_icon.y = def_icon.hgt * (def_icon.start.y + 3 / 2);
+    def_icon.x = def_icon.wdt * (def_icon.start.x + 3 / 2);
+  }
+  target.x = event.pageX - (target.DOM).offsetLeft;
+  target.y = event.pageY - (target.DOM).offsetTop;
+  if(count.click == 1){
+    icon_release();
+    input_attr(target.icon);
+  }else{
+    count.click += 1;
+    setTimeout(()=>{
+      count.click = 0;
+    }, reset_delay);
+  }
+}
+
