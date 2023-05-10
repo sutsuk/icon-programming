c07754ddee011afddc4f2c2146a7bb5a306d7fc9
sutsuk <107033259+sutsuk@users.noreply.github.com>
2023-05-10 17:16:04 +0900

    Update attr.js
    Added functions to set attributes.

--- src/js/attr.js
+++ src/js/attr.js
@@ -530,3 +530,307 @@ function add_attr_func(name, args, ret, begin){
   data.funcs.push(new Func(name, args, ret, begin));
 }
 
+function set_attr_error(error){
+  console.log("[CALL] set_attr_error()");
+  msg(error);
+  console.log("[ERROR] "+error);
+}
+
+function set_attr_int(){
+  console.log("[CALL] set_attr_int()");
+  target.name = $id("int-name").value;
+  target.name = target.name.match(/^[a-zA-Z_]{1}[a-zA-Z0-9_]*/);
+  target.attr.name = $id("int-name").value;
+  if(target.name && target.name[0] == target.attr.name){
+    data.icons[target.icon].attr.name = target.attr.name;
+    add_attr_numvar(target.attr.name);
+  }else{
+    set_attr_error(text.wrongvar);
+  }
+}
+
+function set_attr_double(){
+  console.log("[CALL] set_attr_double()");
+  target.name = $id("double-name").value;
+  target.name = target.name.match(/^[a-zA-Z_]{1}[a-zA-Z0-9_]*/);
+  target.attr.name = $id("double-name").value;
+  if(target.name && target.name[0] == target.attr.name){
+    data.icons[target.icon].attr.name = target.attr.name;
+    add_attr_numvar(target.attr.name);
+  }else{
+    set_attr_error(text.wrongvar);
+  }
+}
+
+function set_attr_char(){
+  console.log("[CALL] set_attr_char()");
+  target.name = $id("char-name").value;
+  target.name = target.name.match(/^[a-zA-Z_]{1}[a-zA-Z0-9_]*/);
+  target.attr.name = $id("char-name").value;
+  if(target.name && target.name[0] == target.attr.name){
+    data.icons[target.icon].attr.name = target.attr.name;
+    add_attr_chrvar(target.attr.name);
+  }else{
+    set_attr_error(text.wrongvar);
+  }
+}
+
+function set_attr_print(){
+  console.log("[CALL] set_attr_print()");
+  target.type = $id("print-type").value;
+  if(target.type == "prm"){
+    target.attr.val.v1 = $id("print-param").value;
+    if(target.attr.val.v1 == ""){
+      set_attr_error(text.novar);
+    }else{
+      data.icons[target.icon].attr.name   = "dummy";
+      data.icons[target.icon].attr.val.t1 = "var";
+      data.icons[target.icon].attr.val.v1 = target.attr.val.v1;
+    }
+  }else if(target.type == "vle"){
+    target.attr.val.v1 = $id("print-string").value;
+    data.icons[target.icon].attr.name   = "dummy";
+    data.icons[target.icon].attr.val.t1 = "val";
+    data.icons[target.icon].attr.val.v1 = target.attr.val.v1;
+  }else{
+    set_attr_error(text.wrongtype);
+  }
+}
+
+function set_attr_scan(){
+  console.log("[CALL] set_attr_scan()");
+  target.name = $id("scan-param").value;
+  if(target.name == ""){
+    set_attr_error(text.novar);
+  }else{
+    data.icons[target.icon].attr.name = target.name;
+  }
+}
+
+function set_attr_if(){
+  console.log("[CALL] set_attr_if()");
+  target.type = $id("if-type").value;
+  if(target.type == "tof"){
+    target.attr.val.v1 = $id("if-value").value;
+    if(target.attr.val.v1 == "true" || target.attr.val.v1 == "false"){
+      data.icons[target.icon].attr.name   = "dummy";
+      data.icons[target.icon].attr.val.t1 = "tof";
+      data.icons[target.icon].attr.val.v1 = target.attr.val.v1;
+    }else{
+      set_attr_error(text.nottof);
+    }
+  }else if(target.type == "pev"){
+    target.attr.val.v1 = $id("if-param1").value;
+    target.attr.val.v2 = $id("if-value").value;
+    if(target.attr.val.v1 == ""){
+      set_attr_error(text.novar);
+    }else{
+      data.icons[target.icon].attr.name   = "dummy";
+      data.icons[target.icon].attr.val.t1 = "var";
+      data.icons[target.icon].attr.val.v1 = target.attr.val.v1;
+      data.icons[target.icon].attr.val.t2 = "val";
+      data.icons[target.icon].attr.val.v2 = target.attr.val.v2;
+    }
+  }else if(type == "pep"){
+    target.attr.val.v1 = $id("if-param1").value;
+    target.attr.val.v2 = $id("if-param2").value;
+    if(target.attr.val.v1 == "" || target.attr.val.v2 == ""){
+      set_attr_error(text.novar);
+    }else{
+      data.icons[target.icon].attr.name   = "dummy";
+      data.icons[target.icon].attr.val.t1 = "var";
+      data.icons[target.icon].attr.val.v1 = target.attr.val.v1;
+      data.icons[target.icon].attr.val.t2 = "var";
+      data.icons[target.icon].attr.val.v2 = target.attr.val.v2;
+    }
+  }else{
+    set_attr_error(text.wrongtype);
+  }
+}
+
+function set_attr_subst(){
+  console.log("[CALL] set_attr_subst()");
+  target.name = $id("subst-param").value;
+  target.attr.val.v1 = $id("subst-value").value;
+  if(target.name == ""){
+    set_attr_error(text.novar);
+  }else{
+    data.icons[target.icon].attr.name   = target.name;
+    data.icons[target.icon].attr.val.t1 = "val";
+    data.icons[target.icon].attr.val.v1 = target.attr.val.v1;
+  }
+}
+
+function set_attr_loopstart(){
+  console.log("[CALL] set_attr_loopstart()");
+  target.name = $id("loops-name").value;
+  if(target.name == ""){
+    set_attr_error(text.novar);
+  }else{
+    data.icons[target.icon].attr.name = target.name;
+    add_attr_loop(target.name);
+  }
+}
+
+function set_attr_loopbreak(){
+  console.log("[CALL] set_attr_loopbreak()");
+  target.name = $id("loopb-name").value;
+  if(target.name == ""){
+    set_attr_error(text.novar);
+  }else{
+    data.icons[target.icon].attr.name = target.name;
+  }
+}
+
+function set_attr_loopend(){
+  console.log("[CALL] set_attr_loopend()");
+  target.name = $id("loope-name").value;
+  if(target.name == ""){
+    set_attr_error(text.novar);
+  }else{
+    data.icons[target.icon].attr.name = target.name;
+  }
+}
+
+function set_attr_math(){
+  console.log("[CALL] set_attr_math()");
+  target.type = $id("math-symbol").value;
+  data.icons[target.icon].attr.expr = target.type;
+  if(target.type == "plus"){
+    target.attr.expr = $id("math-ptype").value;
+  }else if(target.type == "minus"){
+    target.attr.expr = $id("math-mtype").value;
+  }else if(target.type == "times"){
+    target.attr.expr = $id("math-ttype").value;
+  }else if(target.type == "division"){
+    target.attr.expr = $id("math-dtype").value;
+  }else{
+    set_attr_error(text.nosymbol);
+    return;
+  }
+  if(target.attr.expr == "pp"){
+    target.attr.val.v1 = $id("math-param1").value;
+    target.attr.val.v2 = $id("math-param2").value;
+    target.attr.val.v3 = $id("math-param3").value;
+    if(target.attr.val.v1 == "" || target.attr.val.v2 == "" || target.attr.val.v3 == ""){
+      set_attr_error(text.novar);
+    }else{
+      data.icons[target.icon].attr.name   = "dummy";
+      data.icons[target.icon].attr.val.t1 = "var";
+      data.icons[target.icon].attr.val.v1 = target.attr.val.v1;
+      data.icons[target.icon].attr.val.t2 = "var";
+      data.icons[target.icon].attr.val.v2 = target.attr.val.v2;
+      data.icons[target.icon].attr.val.t3 = "var";
+      data.icons[target.icon].attr.val.v3 = target.attr.val.v3;
+    }
+  }else if(target.attr.expr == "pv"){
+    target.attr.val.v1 = $id("math-param1").value;
+    target.attr.val.v2 = $id("math-param2").value;
+    target.attr.val.v3 = $id("math-value1").value;
+    if(target.attr.val.v1 == "" || target.attr.val.v2 == ""){
+      set_attr_error(text.novar);
+    }else{
+      data.icons[target.icon].attr.name   = "dummy";
+      data.icons[target.icon].attr.val.t1 = "var";
+      data.icons[target.icon].attr.val.v1 = target.attr.val.v1;
+      data.icons[target.icon].attr.val.t2 = "var";
+      data.icons[target.icon].attr.val.v2 = target.attr.val.v2;
+      data.icons[target.icon].attr.val.t3 = "val";
+      data.icons[target.icon].attr.val.v3 = target.attr.val.v3;
+    }
+  }else if(target.attr.expr == "vv"){
+    target.attr.val.v1 = $id("math-param1").value;
+    target.attr.val.v2 = $id("math-value1").value;
+    target.attr.val.v3 = $id("math-value2").value;
+    data.icons[target.icon].attr.name   = "dummy";
+    data.icons[target.icon].attr.val.t1 = "var";
+    data.icons[target.icon].attr.val.v1 = target.attr.val.v1;
+    data.icons[target.icon].attr.val.t2 = "val";
+    data.icons[target.icon].attr.val.v2 = target.attr.val.v2;
+    data.icons[target.icon].attr.val.t3 = "val";
+    data.icons[target.icon].attr.val.v3 = target.attr.val.v3;
+  }else{
+    set_attr_error(text.nosymbol);
+  }
+}
+
+function set_attr_funcf(){
+  console.log("[CALL] set_attr_funcf()");
+  var attr = new Object();
+  target.name = $id("funcf-name").value;
+  target.name = target.name.match(/^[a-zA-Z_]{1}[a-zA-Z0-9_]*/);
+  if(target.name && target.name[0] == target.name.input){
+    data.icons[target.icon].attr.name   = target.name.input;
+    data.icons[target.icon].attr.val.t1 = "dest";
+    data.icons[target.icon].attr.val.v1 = data.icons.length;
+    data.icons[target.icon].attr.val.t2 = "args";
+    data.icons[target.icon].attr.val.v2 = new Array();
+    target.attr.val.v2 = $cl("funcf-args");
+    for(var i = 0; i < target.attr.val.v2.length; i++){
+      if(target.attr.val.v2[i].checked){
+        (data.icons[target.icon].attr.val.v2).push(target.attr.val.v2[i].value);
+      }
+    }
+    target.attr.val.v3 = $id("funcf-ret").value;
+    data.icons[target.icon].attr.val.t3 = "ret";
+    data.icons[target.icon].attr.val.v3 = target.attr.val.v3;
+    for(var i = 0; i < def_icon.icons.length; i++){
+      if(def_icon.icons[i].tag == "funct"){
+        target.attr = new Attr();
+        target.attr.name = target.name.input;
+        icon_summon(i, -1, -1, cpyObj(target.attr));
+      }else if(def_icon.icons[i].tag == "funce"){
+        target.attr = new Attr();
+        target.attr.val.t1 = "dest";
+        target.attr.val.v1 = target.icon;
+        target.attr.val.v3 = target.name.input;
+        icon_summon(i, -1, -1, cpyObj(target.attr));
+      }
+    }
+    attr.name  = target.name.input
+    attr.args  = data.icons[target.icon].attr.val.v2;
+    attr.ret   = data.icons[target.icon].attr.val.v3;
+    attr.begin = data.icons.length - 2;
+    add_attr_func(attr.name, attr.args, attr.ret, attr.begin);
+  }else{
+    set_attr_error(text.wrongfunc);
+  }
+}
+
+function set_attr_funce(){
+  console.log("[CALL] set_attr_funce()");
+  target.attr.val.v2 = $id("funce-ret").value;
+  data.icons[target.icon].attr.name = data.icons[target.icon].attr.val.v3;
+  data.icons[target.icon].attr.val.t2 = "ret";
+  data.icons[target.icon].attr.val.v2 = target.attr.val.v2;
+}
+
+function set_attr(){
+  console.log("[CALL] set_attr()");
+  target.attr = cpyObj(data.icons[target.icon].attr);
+  switch(target.type){
+    case "start":     view_attr_start();    break;
+    case "end":       view_attr_vain();     break;
+    case "int":       set_attr_int();       break;
+    case "double":    set_attr_double();    break;
+    case "char":      set_attr_char();      break;
+    case "print":     set_attr_print();     break;
+    case "scan":      set_attr_scan();      break;
+    case "if":        set_attr_if();        break;
+    case "up":        view_attr_vain();     break;
+    case "down":      view_attr_vain();     break;
+    case "rightup":   view_attr_vain();     break;
+    case "rightdown": view_attr_vain();     break;
+    case "right":     view_attr_vain();     break;
+    case "conf":      view_attr_vain();     break;
+    case "subst":     set_attr_subst();     break;
+    case "loopstart": set_attr_loopstart(); break;
+    case "loopbreak": set_attr_loopbreak(); break;
+    case "loopend":   set_attr_loopend();   break;
+    case "math":      set_attr_math();      break;
+    case "funcf":     set_attr_funcf();     break;
+    case "funct":     view_attr_vain();     break;
+    case "funce":     set_attr_funce();     break;
+  }
+}
+
